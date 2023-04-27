0b9db4437500707ba73c511fa9d28d206a5897f4
Abin56 <abinjohn8089@gmail.com>
2023-04-27 23:04:07 +0530

    Students info and Genrate TC completed

--- lib/ui team/sruthi/parent_guardian_alert_box_widget.dart
+++ lib/ui team/sruthi/parent_guardian_alert_box_widget.dart
@@ -1,142 +1,163 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dujo_kerala_website/ui%20team/sruthi/student_details_alert_box_widget.dart';
 import 'package:dujo_kerala_website/view/web/widgets/Iconbackbutton.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../../controller/admin_login_screen/admin_login_screen_controller.dart';
 import '../../view/colors/colors.dart';
 import '../../view/constant/constant.dart';
 
-class Parent_Guardian_Alert_box_Widget extends StatelessWidget {
-  const Parent_Guardian_Alert_box_Widget({
-    super.key,
-        required this.text,
+class ParentAlert_box_Widget extends StatelessWidget {
+  String studentID;
 
+  ParentAlert_box_Widget({
+    required this.studentID,
+    super.key,
+    required this.text,
   });
   final String text;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 300,height: 500,
+      width: 300,
+      height: 500,
       child: AlertDialog(
-        title: Container(
-     
-            color: adminePrimayColor,
-            child: Row(
-              children: [
-                IconButtonBackWidget(color: cWhite),
-                Text('Parent Details',
-                    style: GoogleFonts.poppins(fontSize: 16.h, color: cWhite)),
-              ],
-            )),
-        content: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            
-            SingleChildScrollView(
+          title: Container(
+              color: adminePrimayColor,
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  CircleAvatar(
-                    radius: 60,
-                  )
+                  IconButtonBackWidget(color: cWhite),
+                  Text('Parent Details',
+                      style:
+                          GoogleFonts.poppins(fontSize: 16.h, color: cWhite)),
                 ],
-              ),
-            ),
-                    Student_Info_text_Widget(text: "Name : Anu"),
-            sizedBoxH10,
-    
-          Student_Info_text_Widget(text: "Phone No. :9654781287"),
-    
-            sizedBoxH10,
-                    Student_Info_text_Widget(text: "Gender :Female"),
-            sizedBoxH10,
-    
-                   Student_Info_text_Widget(text: "Blood Group :B -ve"),
-            sizedBoxH10,
-    
-                   Student_Info_text_Widget(text: "Email :anu@gmail.com"),
-            sizedBoxH10,
-                    Student_Info_text_Widget(text: "House Name :New HOme"),
-            sizedBoxH10,
-    
-                    Student_Info_text_Widget(text: "Place :Kochi"),
-            sizedBoxH10,
-                    Student_Info_text_Widget(text: "District :Ernakulam"),
-    
-            sizedBoxH10,
-                   Student_Info_text_Widget(text: "State :Kerala"),
-            sizedBoxH10,
-    
-    
-          ],
-        ),
-        actions: [
-     Student_Info_Elevated_button_Widget(onPressed: () { Navigator.of(context).pop(); }, text: 'Cancel',)
-    
-         ] ),
+              )),
+          content: StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection("SchoolListCollection")
+                  .doc(Get.find<AdminLoginScreenController>().schoolID)
+                  .collection("Students_Parents")
+                  .where('studentID', isEqualTo: studentID)
+                  .snapshots(),
+              builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                  return Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SingleChildScrollView(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CircleAvatar(
+                            radius: 60,
+                            backgroundImage: NetworkImage(snapshot.data!.docs[0]['profileImageURL']??""),
+                          )
+                        ],
+                      ),
+                    ),
+                    Student_Info_text_Widget(
+                        text: "Name : ${snapshot.data!.docs[0]['parentName']}"),
+                    sizedBoxH10,
+                    Student_Info_text_Widget(
+                        text:
+                            "Phone No. :${snapshot.data!.docs[0]['parentPhoneNumber']}"),
+                    sizedBoxH10,
+                    Student_Info_text_Widget(
+                        text: "Gender :${snapshot.data!.docs[0]['gender']}"),
+                    sizedBoxH10,
+                    Student_Info_text_Widget(
+                        text:
+                            "Email :${snapshot.data!.docs[0]['parentEmail']}"),
+                    sizedBoxH10,
+                    Student_Info_text_Widget(
+                        text:
+                            "House Name :${snapshot.data!.docs[0]['houseName']}"),
+                    sizedBoxH10,
+                    Student_Info_text_Widget(
+                        text: "Place :${snapshot.data!.docs[0]['place']}"),
+                    sizedBoxH10,
+                  ],
+                );
+              }else{
+                return const Center(child:  CircularProgressIndicator.adaptive());
+              }
+              }),
+          actions: [
+            Student_Info_Elevated_button_Widget(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              text: 'Cancel',
+            )
+          ]),
     );
-      
-      
-    
   }
 }
 
 class Student_Info_Elevated_button_Widget extends StatelessWidget {
-  const Student_Info_Elevated_button_Widget({
+  Student_Info_Elevated_button_Widget({
     super.key,
     required this.text,
-    required this.onPressed
-
- 
+    required this.onPressed,
   });
-final String text;
- final VoidCallback onPressed;
+  final String text;
+  final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-            onPressed: onPressed,style: ElevatedButton.styleFrom(
-    backgroundColor: adminePrimayColor, // Background color
-  ),
-            child: Text(text));
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: adminePrimayColor, // Background color
+        ),
+        child: Text(text));
   }
 }
 
 class Student_Info_text_Widget extends StatelessWidget {
-  const Student_Info_text_Widget({
-    super.key,
-    required this.text
-  });
+  const Student_Info_text_Widget({super.key, required this.text});
   final String text;
 
   @override
   Widget build(BuildContext context) {
-    return Text(text,
-        style: GoogleFonts.poppins(fontSize: 14));
+    return Text(text, style: GoogleFonts.poppins(fontSize: 14));
   }
 }
-void _showParentAlertbox(BuildContext context) {
+
+void _showParentAlertbox(
+    BuildContext context, studentID, String firebaseCollection) {
   showDialog(
     context: context,
-    builder: (context) => Parent_Guardian_Alert_box_Widget(text: 'Parent Info',),
-  );
-}
-void _showGuardianAlertbox(BuildContext context) {
-  showDialog(
-    context: context,
-    builder: (context) => Parent_Guardian_Alert_box_Widget(text: 'Parent Info',),
+    builder: (context) => ParentAlert_box_Widget(
+      studentID: studentID,
+      text: 'Parent Info',
+    ),
   );
 }
 
-void _showAlert(BuildContext context) {
-  var screenSize = MediaQuery.of(context).size;
+void _showGuardianAlertbox(
+    BuildContext context, String studentID, String firebaseCollection) {
   showDialog(
     context: context,
-    builder: (context) => Student_Details_AlertBox_Widget(),
+    builder: (context) => ParentAlert_box_Widget(
+      studentID: studentID,
+      text: 'Parent Info',
+    ),
   );
 }
+
+// void _showAlert(BuildContext context) {
+//   var screenSize = MediaQuery.of(context).size;
+//   showDialog(
+//     context: context,
+//     builder: (context) => Student_Details_AlertBox_Widget(),
+//   );
+// }
 
 
--- lib/view/web/login/admin/admin_DashBoard/all_Students/search_students.dart
+++ lib/view/web/login/admin/admin_DashBoard/all_Students/search_students.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dujo_kerala_website/view/colors/colors.dart';
 import 'package:dujo_kerala_website/view/constant/constant.dart';
@@ -8,6 +10,7 @@ import 'package:google_fonts/google_fonts.dart';
 
 import '../../../../../../controller/admin_login_screen/admin_login_screen_controller.dart';
 import '../../../../../../controller/search_student/search_student.dart';
+import '../../../../../../model/create_classModel/addStudent_model.dart';
 import '../../../../../../ui team/sruthi/parent_guardian_alert_box_widget.dart';
 import '../../../../../../ui team/sruthi/student_details_alert_box_widget.dart';
 
@@ -32,7 +35,84 @@ class SearchStuents extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    throw UnimplementedError();
+    return StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection("SchoolListCollection")
+            .doc(Get.find<AdminLoginScreenController>().schoolID)
+            .collection("AllStudents")
+            .where('admissionNumber', isEqualTo: query)
+            .snapshots(),
+        builder: (context, snapshots) {
+          var screenSize = MediaQuery.of(context).size;
+          if (snapshots.hasData) {
+            return Scaffold(
+              // backgroundColor: Colors.transparent,
+              body: ListView.separated(
+                  itemBuilder: (context, index) {
+                    AddStudentModel data = AddStudentModel.fromMap(
+                        snapshots.data!.docs[index].data());
+                    return Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          // border: Border.all(color: Colors.grey,width: 0.5),
+                        ),
+                        height: screenSize.width / 8,
+                        width: double.infinity,
+                        child: Row(
+                          children: [
+                            GestureDetector(
+                                onTap: () {
+                                  log(data.profileImageUrl ?? "mmmmmmmmmm");
+                                  _showlert(context, data);
+                                },
+                                child: CircleAvatar(
+                                  radius: 60,
+                                )),
+                            sizedBoxw40,
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  // Text(snapshots.data!.docs[index]['id']),
+                                  Text(
+                                    data.studentName!,
+                                    style: GoogleFonts.poppins(fontSize: 16),
+                                  ),
+                                  sizedBoxH10,
+                                  Text(
+                                    'Admission No. :${data.admissionNumber}',
+                                    style: GoogleFonts.poppins(fontSize: 12),
+                                  ),
+                                  sizedBoxH10,
+
+                                  Text(
+                                    'Class & Division : ${data.classID}',
+                                    style: GoogleFonts.poppins(fontSize: 12),
+                                  ),
+                                  sizedBoxH10,
+                                  Text(
+                                    'Phone No :${data.guardianID}',
+                                    style: GoogleFonts.poppins(fontSize: 12),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        ));
+                  },
+                  separatorBuilder: (context, index) {
+                    return const Divider();
+                  },
+                  itemCount: snapshots.data!.docs.length),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator.adaptive(),
+            );
+          }
+        });
+    ;
   }
 
   @override
@@ -51,6 +131,8 @@ class SearchStuents extends SearchDelegate {
                   // backgroundColor: Colors.transparent,
                   body: ListView.separated(
                       itemBuilder: (context, index) {
+                        AddStudentModel data = AddStudentModel.fromMap(
+                            snapshots.data!.docs[index].data());
                         return Container(
                             decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(10),
@@ -62,7 +144,8 @@ class SearchStuents extends SearchDelegate {
                               children: [
                                 GestureDetector(
                                     onTap: () {
-                                      _showlert(context);
+                                      log(data.profileImageUrl ?? "mmmmmmmmmm");
+                                      _showlert(context, data);
                                     },
                                     child: CircleAvatar(
                                       radius: 60,
@@ -76,26 +159,26 @@ class SearchStuents extends SearchDelegate {
                                     children: [
                                       // Text(snapshots.data!.docs[index]['id']),
                                       Text(
-                                        'Abin ',
+                                        data.studentName!,
                                         style:
                                             GoogleFonts.poppins(fontSize: 16),
                                       ),
                                       sizedBoxH10,
                                       Text(
-                                        'Admission No. :7539',
+                                        'Admission No. :${data.admissionNumber}',
                                         style:
                                             GoogleFonts.poppins(fontSize: 12),
                                       ),
                                       sizedBoxH10,
 
                                       Text(
-                                        'Class & Division : 10 B',
+                                        'Class & Division : ${data.classID}',
                                         style:
                                             GoogleFonts.poppins(fontSize: 12),
                                       ),
                                       sizedBoxH10,
                                       Text(
-                                        'Phone No :7539857469',
+                                        'Phone No :${data.guardianID}',
                                         style:
                                             GoogleFonts.poppins(fontSize: 12),
                                       ),
@@ -124,16 +207,68 @@ class SearchStuents extends SearchDelegate {
                 .where('admissionNumber', isEqualTo: query)
                 .snapshots(),
             builder: (context, snapshots) {
+              var screenSize = MediaQuery.of(context).size;
               if (snapshots.hasData) {
                 return Scaffold(
-                  backgroundColor: Colors.transparent,
+                  // backgroundColor: Colors.transparent,
                   body: ListView.separated(
                       itemBuilder: (context, index) {
+                        AddStudentModel data = AddStudentModel.fromMap(
+                            snapshots.data!.docs[index].data());
                         return Container(
-                          height: 100,
-                          width: double.infinity,
-                          child: Text(snapshots.data!.docs[index]['id']),
-                        );
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              // border: Border.all(color: Colors.grey,width: 0.5),
+                            ),
+                            height: screenSize.width / 8,
+                            width: double.infinity,
+                            child: Row(
+                              children: [
+                                GestureDetector(
+                                    onTap: () {
+                                      log(data.profileImageUrl ?? "mmmmmmmmmm");
+                                      _showlert(context, data);
+                                    },
+                                    child: CircleAvatar(
+                                      radius: 60,
+                                    )),
+                                sizedBoxw40,
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      // Text(snapshots.data!.docs[index]['id']),
+                                      Text(
+                                        data.studentName!,
+                                        style:
+                                            GoogleFonts.poppins(fontSize: 16),
+                                      ),
+                                      sizedBoxH10,
+                                      Text(
+                                        'Admission No. :${data.admissionNumber}',
+                                        style:
+                                            GoogleFonts.poppins(fontSize: 12),
+                                      ),
+                                      sizedBoxH10,
+
+                                      Text(
+                                        'Class & Division : ${data.classID}',
+                                        style:
+                                            GoogleFonts.poppins(fontSize: 12),
+                                      ),
+                                      sizedBoxH10,
+                                      Text(
+                                        'Phone No :${data.guardianID}',
+                                        style:
+                                            GoogleFonts.poppins(fontSize: 12),
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ));
                       },
                       separatorBuilder: (context, index) {
                         return const Divider();
@@ -149,12 +284,21 @@ class SearchStuents extends SearchDelegate {
   }
 }
 
-
-
-void _showlert(BuildContext context) {
-
+void _showlert(BuildContext context, AddStudentModel data) {
   showDialog(
-    barrierDismissible: false,
+      barrierDismissible: false,
       context: context,
-      builder: (context) => Student_Details_AlertBox_Widget());
+      builder: (context) => Student_Details_AlertBox_Widget(
+        studentID: data.docid??"",
+            studentImage: data.profileImageUrl ?? "",
+            studentName: data.studentName ?? "",
+            studentClass: data.classID ?? "",
+            admissionNumber: data.admissionNumber ?? "",
+            studentGender: data.gender ?? "",
+            bloodGroup: data.bloodgroup ?? "",
+            studentEmail: data.studentemail ?? "",
+            houseName: data.houseName ?? "",
+            place: data.place ?? "",
+            district: data.district ?? "",
+          ));
 }
