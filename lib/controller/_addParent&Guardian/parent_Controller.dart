334547401d26cb7e12a51bd9d719d6626f91f16d
Praveen <praveenpathmakaran@gmail.com>
2023-05-15 18:36:46 +0530

    bug fixed

--- lib/controller/_addParent&Guardian/parent_Controller.dart
+++ lib/controller/_addParent&Guardian/parent_Controller.dart
@@ -6,6 +6,7 @@ import 'package:dujo_kerala_website/view/constant/constant.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../view/web/widgets/drop_DownList/get_students.dart';
 import '../../view/web/widgets/drop_DownList/schoolDropDownList.dart';
 import '../get_firebase-data/get_firebase_data.dart';
 
@@ -25,7 +26,8 @@ class ParentController extends GetxController {
         studentID: studentDocID,
         createdate: DateTime.now().toString(),
         docid: uuid.v1());
-    firebaseFirestore.collection(Get.find<GetFireBaseData>().bYear.value)
+    firebaseFirestore
+        .collection(Get.find<GetFireBaseData>().bYear.value)
         .doc(Get.find<GetFireBaseData>().bYear.value)
         .collection("classes")
         .doc(classID)
@@ -33,9 +35,10 @@ class ParentController extends GetxController {
         .doc(data.docid)
         .set(data.toMap())
         .then((value) {
-          parentName.clear();
-          parentPhoneNumber.clear();
-      
+      parentName.clear();
+      parentPhoneNumber.clear();
+      schoolStudentListValue = null;
+
       showToast(msg: "Added");
     });
   }
