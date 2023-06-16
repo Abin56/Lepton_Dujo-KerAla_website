3e2f9c5e365a168ea84aebceabca1efcd01bd321
Praveen <praveenpathmakaran@gmail.com>
2023-06-16 12:31:33 +0530

    create bus route function added

--- lib/view/web/login/admin/admin_DashBoard/bus_Route/create_bus_route.dart
+++ lib/view/web/login/admin/admin_DashBoard/bus_Route/create_bus_route.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
 
+import '../../../../../../controller/bus_route_controller/bus_route_controller.dart';
 import '../../../../../colors/colors.dart';
 import '../../../../../constant/constant.dart';
 import '../../../../../fonts/fonts.dart';
 
 class CreateBusRoute extends StatelessWidget {
-  const CreateBusRoute({super.key});
-  //  TextEditingController routeController = TextEditingController();
-  //   TextEditingController busnumberController = TextEditingController();
-  //    TextEditingController driverphoneController = TextEditingController();
-  //     TextEditingController assistancephoneController = TextEditingController();
-  //      TextEditingController staffinchargeController = TextEditingController();
+  CreateBusRoute({super.key});
+  final TextEditingController routeController = TextEditingController();
+  final TextEditingController busnumberController = TextEditingController();
+  final TextEditingController driverphoneController = TextEditingController();
+  final TextEditingController assistancephoneController =
+      TextEditingController();
+  final TextEditingController staffinchargeController = TextEditingController();
+  final BusRouteController busRouteController = Get.put(BusRouteController());
 
   @override
   Widget build(BuildContext context) {
@@ -86,32 +90,32 @@ class CreateBusRoute extends StatelessWidget {
                         padding: EdgeInsets.only(
                             left: size.width / 10, right: size.width / 10),
                         child: Column(children: [
-                          const BusRouteTextFormWidget(
-                            //  textEditingController:routeController,
+                          BusRouteTextFormWidget(
+                            textEditingController: routeController,
                             function: checkFieldEmpty,
                             labelText: 'Route Number',
                             icon: Icons.route_outlined,
                           ),
-                          const BusRouteTextFormWidget(
-                            //textEditingController:busnumberController ,
+                          BusRouteTextFormWidget(
+                            textEditingController: busnumberController,
                             function: checkFieldEmpty,
                             labelText: 'Bus Number',
                             icon: Icons.bus_alert,
                           ),
-                          const BusRouteTextFormWidget(
-                            // textEditingController:driverphoneController,
+                          BusRouteTextFormWidget(
+                            textEditingController: driverphoneController,
                             function: checkFieldPhoneNumberIsValid,
-                            labelText: 'Driver Mobile Numbe',
+                            labelText: 'Driver Mobile Number',
                             icon: Icons.phone_android_sharp,
                           ),
-                          const BusRouteTextFormWidget(
-                            // textEditingController: assistancephoneController,
+                          BusRouteTextFormWidget(
+                            textEditingController: assistancephoneController,
                             function: checkFieldPhoneNumberIsValid,
                             labelText: 'Assistance Mobile Number',
                             icon: Icons.phone_android,
                           ),
-                          const BusRouteTextFormWidget(
-                            // textEditingController:staffinchargeController ,
+                          BusRouteTextFormWidget(
+                            textEditingController: staffinchargeController,
                             function: checkFieldEmpty,
                             labelText: 'Staff inCharge',
                             icon: Icons.person_2,
@@ -135,14 +139,31 @@ class CreateBusRoute extends StatelessWidget {
                                   textStyle: const TextStyle(fontSize: 17),
                                 ),
                                 onPressed: () async {
-                                  bool? result =
-                                      formKey.currentState?.validate();
-                                  if (result == true) {
-                                    showToast(
-                                        msg: 'Bus route successfully created ');
+                                  if (formKey.currentState?.validate() ??
+                                      false) {
+                                    await busRouteController
+                                        .createBusRoute(
+                                            routeNumber: routeController.text,
+                                            busNumber: busnumberController.text,
+                                            driveMobNum:
+                                                driverphoneController.text,
+                                            assistantMobNum:
+                                                assistancephoneController.text,
+                                            staffInCharge:
+                                                staffinchargeController.text)
+                                        .then((value) {
+                                      routeController.clear();
+                                      busnumberController.clear();
+                                      driverphoneController.clear();
+                                      assistancephoneController.clear();
+                                      staffinchargeController.clear();
+                                    });
                                   }
                                 },
-                                child: const Text('Create'),
+                                child: Obx(() =>
+                                    busRouteController.isLoading.value
+                                        ? circularProgressIndicator
+                                        : const Text('Create')),
                               ),
                             ),
                           ),
@@ -164,11 +185,11 @@ class BusRouteTextFormWidget extends StatelessWidget {
   const BusRouteTextFormWidget({
     super.key,
     required this.labelText,
-    // required this.textEditingController,
+    required this.textEditingController,
     required this.function,
     required this.icon,
   });
-  // final TextEditingController textEditingController;
+  final TextEditingController textEditingController;
   final String labelText;
   final String? Function(String? fieldContent) function;
   final IconData icon;
@@ -179,7 +200,7 @@ class BusRouteTextFormWidget extends StatelessWidget {
       padding: const EdgeInsets.all(15),
       child: TextFormField(
         validator: function,
-        // controller: textEditingController,
+        controller: textEditingController,
         decoration: InputDecoration(
           fillColor: cWhite,
           border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
