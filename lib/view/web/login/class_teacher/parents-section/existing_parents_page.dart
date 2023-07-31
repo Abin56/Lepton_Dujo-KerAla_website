bdd8f57245d16fa7da75f94ecae6d0a780dd7b01
Abin56 <abinjohn8089@gmail.com>
2023-07-31 13:59:44 +0530

    new commit

---
+++ lib/view/web/login/class_teacher/parents-section/existing_parents_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ExistingParentsPage extends StatelessWidget {
+  const ExistingParentsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
